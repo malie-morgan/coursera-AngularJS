9e7314c9771e6e01e0952e54ee274967064e4bb1
Malie Morgan <mcmorgan273@gmail.com>
2019-10-04 15:42:10 -0600

    mod1-solution

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -1,32 +1,26 @@
 (function () {
-'use strict';
+    'use strict';
+    angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
 
-angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
-
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-    $scope.dishes = "";
-    $scope.message = "";
-    $scope.data = false;
-
-    $scope.checkIfTooMuch = function () {
-        if ($scope.dishes.trim().length === 0) {
-            $scope.data = false;
-        }
-        else {
-            $scope.data = true;
+    function LunchCheckController($scope) {
+        $scope.dishes = "";
+        $scope.message = "";
+        $scope.data = false;
+        $scope.checkIfTooMuch = function () {
+            if ($scope.dishes.trim().length === 0) {
+                $scope.data = false;
+            }
+            else {
+                $scope.data = true;
                 if ($scope.dishes.trim().length <= 3) {
                     $scope.message = "Enjoy!";
                 }
                 else {
                     $scope.message = "Too much!";
                 }
-
+            }
         }
-
     }
-}
-
-
-});
+})();
 
