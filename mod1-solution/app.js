17926cbac6a8a3de18ecba348c57fad125b99463
Malie Morgan <mcmorgan273@gmail.com>
2019-10-08 17:04:43 -0600

    mod1-solution

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -6,8 +6,8 @@
     function LunchCheckController($scope) {
         $scope.dishes = "";
        // $scope.message = "";
-       // $scope.data = false;
-       // $scope.error = false;
+        $scope.data = false;
+        $scope.error = false;
         $scope.checkIfTooMuch = function () {
             if ($scope.dishes.trim().split(', ').length <= 0) {
                 $scope.message = "Please enter lunch dishes first!"
@@ -17,7 +17,7 @@
             else {
                 $scope.data = true;
                 $scope.error = false;
-                if ($scope.dishes.trim().split(', ').length > 0 && <= 3) {
+                if ($scope.dishes.trim().split(', ').length > 0 && $scope.dishes.trim().split(', ').length <= 3) {
                     $scope.message = "Enjoy!";
                 }
                 else {
