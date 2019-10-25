f4a09b7f525fdc7d5f7d247d7485bd4c74457ae3
Malie Morgan <mcmorgan273@gmail.com>
2019-10-24 19:05:51 -0600

    Update 3

--- mod3-solution/app.js
+++ mod3-solution/app.js
@@ -1,34 +1,62 @@
 (function () {
     'use strict';
 
-    angular.module('NarrowItDOwnApp', [])
+    angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
-        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");  //API base path constant: define base URL once and reuse it in dif circumstances using special function called constant. Takes name and base path
+        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")  //API base path constant: define base URL once and reuse it in dif circumstances using special function called constant. Takes name and base path
+        .directive('foundItems', FoundItemsDirective);
 
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: 'foundItems.html',
+            scope: {
+                items: '<',
+                onRemove: '&',   //reference to the function such that its going to get executed by the parent environment
+            },
+            controller: NarrowItDownController,
+            controllerAs: 'menu',
+            bindToController: true,
+           //link: FoundItemsDirectiveLink,
+            transclude: true
+        };
+
+        return ddo;
+    }
 
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
         var menu = this;
+        menu.foundItems = [];
+        var search = "";
 
-        var promise = MenuSearchService.getMatchedMenuItems();    //call menu categories service that gets menu categories which returns a promise
+        menu.matchedMenuItems = function(searchTerm) {
+            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);      //call menu categories service that gets menu categories which returns a promise
+            /* promise.then(function (response) {                          //resolve promise using then function, expecting a response
+                 menu.Items = response.data;                             //or response.data to be that Json converted into array of obj literals. Then assign that array to the categories property of our menu
+             });*/
 
-        promise.then(function (response) {                          //resolve promise using then function, expecting a response
-            menu.Items = response.data;                          //or response.data to be that Json converted into array of obj literals. Then assign that array to the categories property of our menu
-        })
-            .catch(function (error) {                                   //catch all function
-                console.log("Something went terribly wrong.");
+            promise.then(function (items) {
+                if (items && items.length > 0) {
+                    menu.warning = '';
+                    menu.foundItems = items;
+                } else {
+                    menu.warning = 'Nothing found!';
+                    menu.foundItems = [];
+                }
             });
+        };
 
-        menu.logMenuItems = function (shortName) {                            //logMenuItems function taking shortName as argument
-            var promise = MenuSearchService.getMenuForSearch(shortName);  //calling MenuCategoriesService.getMenuForCategory method and passing it shortName
+        menu.removeItem = function (itemIndex) {        //remove item from found array
+            menu.foundItems.splice(itemIndex, 1);
+        };
+       /* menu.nothingFound = function(){
+            return menu.foundItems.length <= 0;
+        };*/
+        menu.warning = "";                          //////
 
-            promise.then(function (response) {                                   //logging response on success
-                console.log(response.data);
-            })
-                .catch(function (error) {                                             //logging error on failure
-                    console.log(error);
-                })
+ /*       menu.getItems = function () {
+            return items;
         };
 
     }
@@ -36,31 +64,35 @@
 
     MenuSearchService.$inject = ['$http', 'ApiBasePath'];   //injecting http service
     function MenuSearchService($http, ApiBasePath) {
-        var service = this;                         //creates this service local variable equal to this
+        var service = this;                                 //creates this service local variable equal to this
 
-        service.getMatchedMenuItems = function () {   //getMenuCategories is our main method
+/*        service.getMatchedMenuItems = function () {   //getMatchedMenuItem is the main method
             var response = $http({                    //use our http service and configure it using the config object ({})
                 method: "GET",                          //with a method of GET which will tell the http service to issue a get request
-                url: (ApiBasePath + "/categories.json") //Appending URL to our API base path
+                url: (ApiBasePath + "/menu_items.json") //Appending URL to the API base path
             });
 
-            return response;                          //once we capture the response line 42 ( which is a promise), return response to caller
-        };
+            return response;                                //after capturing the response line 42 ( which is a promise), return response to caller
+        };*/
 
+        service.getMatchedMenuItems = function (searchTerm) {         //getMatchedMenuItem is the main method
+            return $http({                                  //use our http service and configure it using the config object ({})
+                method: "GET",                              //with a method of GET which will tell the http service to issue a get request
+                url: (ApiBasePath + "/menu_items.json")
+            }).then(function (response) {
 
-        service.getMenuForCategory = function (shortName) {   //method is getMenuForCategory function that takes shortNsme
-            var response = $http({                              //$http service
-                method: "GET",
-                url: (ApiBasePath + "/menu_items.json"),          //links to .constant function
-                params: {                                         //params property (obj literal ie {}) as part of config object that has
-                    category: shortName                             //category which gets its value shortName that is passed into this function (line 51)
-                }
-            });
+                var foundItems = [];
 
-            return response;                                    //return response which is nothing more than a promise
-        };
-
-    }
-
+                for (var i = 0; i < response.data.menu_items.length; i++) {
+                    //console.log(response.data.menu_items[i]);
+                    if (searchTerm.length > 0 && response.data.menu_items[i].description.toLowerCase().indexOf(searchTerm) !== -1) {
+                        //console.log(response.data.menu_items[i]);
+                        foundItems.push(response.data.menu_items[i]);
+                    }
+            }
+            return foundItems;
+        });
+    };
+}
 })();
 
